a6954e27b2c77afb38a343145344a6b55101225f
alexlhanson <alexlhanson@gmail.com>
2018-09-06 15:41:18 -0700

    Add undefined for empty on pop method

--- lab-alex/__test__/list.test.js
+++ lab-alex/__test__/list.test.js
@@ -2,6 +2,17 @@
 
 const List = require('../lib/list.js');
 
+//create base instance for testing
+let testCase = new List;
+for (let i = 0; i < 5; i++){
+  testCase.push(i);
+}
+
+let emptyCase = new List;
+
+/********************************************************************************
+*         Length Tests                                                          *
+********************************************************************************/
 describe ('list length and instantiation', () => {
   test('should show that it is getting the List constructor with initial length', () => {
     let actual = new List;
@@ -17,6 +28,10 @@ describe ('list length and instantiation', () => {
   })
 })
 
+/********************************************************************************
+*         Push Tests                                                            *
+********************************************************************************/
+
 describe ('list push method', () => {
   test('shows object is instantiated with item and length',() => {
     let actual = new List;
@@ -26,18 +41,32 @@ describe ('list push method', () => {
   })
 
   test('shows item added to bottom of object', () => {
-    let listFive = new List;
-    for (let i = 0; i < 5; i++){
-      listFive.push(i);
-    }
-
-    let actual = listFive.length;
+    let actual = testCase.length;
     let expectedValue = 5;
     expect(actual).toEqual(expectedValue); 
 
-    actual = listFive[4];
+    actual = testCase[4];
     expectedValue = 4
     expect(actual).toBe(expectedValue);
+  })
+
+})
+/********************************************************************************
+*         Pop Tests                                                             *
+********************************************************************************/
+describe ('list pop method', () => {
+
+  test('shows object removes the last item',() => {
+    let popTestCase = testCase;
+    let actual = popTestCase.pop();  
+    let expectedValue = 4;
+    expect(actual).toEqual(expectedValue);
+  })
+
+  test('shows undefined if list is empty',() => {
+    let actual = emptyCase.pop();  
+    let expectedValue = undefined;
+    expect(actual).toEqual(expectedValue);
   })
 
 })
--- lab-alex/lib/list.js
+++ lab-alex/lib/list.js
@@ -11,8 +11,16 @@ class List {
     this.length++;
     return this.length
   }
-    
+  
     //pop method
+  pop(){
+    if (this.length === 0) return undefined; 
+    
+    let poppedItem = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length--;
+    return poppedItem;
+  }
     
     //slice method
     
